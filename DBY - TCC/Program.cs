308db4957954f8df614ca7e544216ce7953bd2f9
Yago Modena <modenayago@yahoo.com>
2023-08-22 23:39:38 -0300

    Adicionar arquivos de projeto.

---
+++ DBY - TCC/Program.cs
@@ -0,0 +1,23 @@
+﻿using DBY___TCC.Formularios.Login;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBY___TCC
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
