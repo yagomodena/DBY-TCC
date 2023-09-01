828aa92eabd67676126d44b4ab6d874f5ba2e9c7
Yago Modena <modenayago@yahoo.com>
2023-08-31 21:59:26 -0300

    Criação da classe de produtos

---
+++ DBY - TCC/Classes/Produto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBY___TCC.Classes
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public virtual Categoria Categoria { get; set; }
+        public string CategoriaId { get; set; }
+        public int MarcaId { get; set; }
+        public virtual Marca Marca { get; set; }
+
+    }
+}
